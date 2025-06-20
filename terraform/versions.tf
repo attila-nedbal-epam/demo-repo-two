8cf025d1e02f2d26867913bedeb0f338affeec54
Attila Nedbal <111579043+attila-nedbal-epam@users.noreply.github.com>
2025-06-20 13:34:22 +0200

    Add Terraform versions and provider constraints

---
+++ terraform/versions.tf
@@ -0,0 +1,12 @@
+# Terraform version and provider constraints
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
